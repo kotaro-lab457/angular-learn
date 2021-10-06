6bd6d4a5415d5cd6c9c37a3f266cf558b401fb3b
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-10-06 14:48:34 +0900

    モジュール分割を利用したルーティング

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductModule } from './product/product.module';
 
 // router でパス, コンポーネントを読み込む
 const routes: Routes = [
-  { path: '', component: ProductListComponent },
-  {path: 'detail', component: ProductDetailComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ProductModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
