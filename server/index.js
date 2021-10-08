8f6991a58f5ec71301c0202af32d6238dd5070bb
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-10-08 17:31:34 +0900

    mongoDB, ルーティングの読み込み

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const config = require('./config/dev');
 const app = express();
-const PORT = process.env.PORT || '3001';
+const config = require('./config/dev');
+const FakeDB = require('./face-db');
+const productRouters = require('./routers/products');
 
-mongoose.connect(config.DB_PATH);
+mongoose.connect(config.DB_PATH)
+.then(() => {
+  const fakeDB = new FakeDB();
+  fakeDB.initDB();
+})
 mongoose.connection.once('open', () => {
   console.log('mongoDB 接続完了');
 })
+
+// APIエンドポイント
+app.use('/api/v1/products', productRouters);
 
 app.get('/products', (req, res) => {
   res.json({'success':true });
 });
 
+const PORT = process.env.PORT || '3001';
 app.listen(PORT, () => {
   console.log('サーバー起動');
 });
